3426519adf6ea7c9527ca4a2a87e3d4d392ed5b4
oleksandr <oleksandrlazebnij8@gmail.com>
2024-03-24 16:06:03 +0200

    3 levels

--- lab2/btree.py
+++ lab2/btree.py
@@ -2,6 +2,7 @@ from collections import deque
 
 D = 2
 
+
 class Node:
     def __init__(self, parent=None, leaf=False):
         self.keys = []
@@ -10,19 +11,39 @@ class Node:
         self.leaf = leaf
 
     def split(self):
-        right_node = Node(leaf=self.leaf)
+        right_node = Node(leaf=True)
         mid_idx = len(self.keys) // 2
         goes_up = self.keys[mid_idx]
 
-        new_root = Node()
         if self.parent is None:
+            new_root = Node()
             new_root.keys.append(goes_up)
             right_node.keys = self.keys[mid_idx + 1:]
             self.keys = self.keys[:mid_idx]
+
+            self.parent = new_root
+
+            if not self.leaf:
+                children_mid_idx = len(self.children) // 2
+                right_node.children = self.children[children_mid_idx+1:]
+                self.children = self.children[:children_mid_idx]
+
+                right_node.parent = new_root
+                right_node.leaf = False
+
             new_root.children = [self, right_node]
+
             for child in new_root.children:
                 child.parent = new_root
-        return new_root
+
+            return new_root
+        else:
+            self.parent.keys.append(goes_up)
+            self.parent.keys.sort()
+            right_node.keys = self.keys[mid_idx + 1:]
+            right_node.parent = self.parent
+            self.keys = self.keys[:mid_idx]
+            self.parent.children += [right_node]
 
     def insert(self, key):
         if self.leaf:
@@ -30,18 +51,29 @@ class Node:
             self.keys.sort()
             if len(self.keys) > D * 2:
                 return self.split()
-            return self
         else:
             index = 0
             while index < len(self.keys) and key >= self.keys[index]:
                 index += 1
-            if self.children and index < len(self.keys) + 1:  # Check if the node has children
-                new_root = self.children[index].insert(key)
-                if new_root:  # If the child node split, handle the split
-                    if self.parent is None:  # If current node is root, update root
-                        self.parent = new_root
-                    else:  # Otherwise, split recursively
-                        return new_root.split()
+
+            # Check if the current node is a leaf
+            if self.children[index].leaf:
+                # If it's a leaf node, insert the key directly
+                self.children[index].keys.append(key)
+                self.children[index].keys.sort()
+                if len(self.children[index].keys) > D * 2:
+                    self.children[index].split()
+            else:
+                # If it's an internal node, continue traversal
+                self.children[index].insert(key)
+                if len(self.children[index].keys) > D * 2:
+                    self.children[index]()
+
+            if len(self.keys) > D * 2:
+                return self.split()
+
+        return self
+
 
     def search(self, key):
         if key in self.keys:
@@ -64,7 +96,6 @@ class BPlusTree:
 
     def insert(self, key):
         self.root = self.root.insert(key)
-
 
     def search(self, key):
         return self.root.search(key)
@@ -92,11 +123,17 @@ class BPlusTree:
 def main():
     B = BPlusTree()
 
-    for i in range(5):
-        B.insert((i, "value"))
+    for i in range(1, 18):
+        B.insert((i, f"value {i}"))
 
     B.print_tree()
 
 
 if __name__ == '__main__':
     main()
+
+# при перенесенні значення в корінь, треба його копіювати (залишати в ноді)
+# при вставці значення 17 воно не опускається до листів
+
+#ліве піддерево показується а праве ні.!!!
+
