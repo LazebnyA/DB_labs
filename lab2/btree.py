76b6c7dd92b5790ec376e3326227c0bcdc5a26f4
oleksandr <oleksandrlazebnij8@gmail.com>
2024-03-29 14:17:17 +0200

    search and search after impl

--- lab2/btree.py
+++ lab2/btree.py
@@ -8,8 +8,8 @@ def hash_function(word):
         'А': 1, 'Б': 1, 'В': 1, 'Г': 2, 'Д': 2, 'Е': 2, 'Є': 2,
         'Ж': 3, 'З': 3, 'И': 3, 'І': 3, 'Ї': 3, 'Й': 3,
         'К': 4, 'Л': 4, 'М': 5, 'Н': 5, 'О': 6, 'П': 6,
-        'Р': 7, 'С': 7, 'Т': 7, 'У': 7, 'Ф': 8, 'Х': 8,
-        'Ц': 8, 'Ч': 8, 'Ш': 8, 'Щ': 9, 'Ь': 9, 'Ю': 9,
+        'Р': 7, 'С': 7, 'Т': 8, 'У': 8, 'Ф': 8, 'Х': 9,
+        'Ц': 9, 'Ч': 9, 'Ш': 9, 'Щ': 9, 'Ь': 9, 'Ю': 9,
         'Я': 9
     }
 
@@ -18,6 +18,8 @@ def hash_function(word):
 
     code = ''
     for char in word:
+        if char.upper() not in dictionary.keys():
+            continue
         code += str(dictionary.get(char.upper()))  # перекодовуємо кожну букву в хеш
     code += '0' * (10 - len(code))  # додаємо нулі до коду, щоб отримати 10 значень
 
@@ -25,11 +27,15 @@ def hash_function(word):
 
 
 class Node:
-    def __init__(self, parent=None, leaf=False):
+    def __init__(self, parent=None, leaf=False, next_sibling=None):
         self.keys = []
-        self.children = [] if not leaf else None
-        self.parent = parent
         self.leaf = leaf
+        self.parent = parent
+        self.children = [] if not leaf else None
+        self.next_sibling = next_sibling
+
+    def get_keys(self):
+        return self.keys
 
     def split(self):
         right_node = Node(leaf=True)
@@ -53,6 +59,7 @@ class Node:
             self.children = self.children[:children_mid_idx]
         else:
             right_node.keys = self.keys[mid_idx:]
+            self.next_sibling = right_node
 
         self.keys = self.keys[:mid_idx]
 
@@ -87,15 +94,12 @@ class Node:
             while index < len(self.keys) and key[0] >= self.keys[index]:
                 index += 1
 
-            # Check if the current node is a leaf
             if self.children[index].leaf:
-                # If it's a leaf node, insert the key directly
                 self.children[index].keys.append(key)
                 self.children[index].keys.sort()
                 if len(self.children[index].keys) > D * 2:
                     self.children[index].split()
             else:
-                # If it's an internal node, continue traversal
                 self.children[index].insert(key)
                 if len(self.children[index].keys) > D * 2:
                     self.children[index]()
@@ -105,19 +109,42 @@ class Node:
 
         return self
 
-    def search(self, key):
-        if key in self.keys:
-            return True
-        elif self.leaf:
-            return False
-        else:
+    def search_object(self, key):
+        if self.leaf and key in [pair[0] for pair in self.keys]:
+            return self
+
+        if not self.leaf:
             index = 0
-            while index < len(self.keys) and key > self.keys[index]:
+            while index < len(self.keys) and key >= self.keys[index]:
                 index += 1
-            if self.children and index < len(self.keys) + 1:  # Check if the node has children
-                return self.children[index].search(key)
-            else:
-                return False
+            if self.children and index < len(self.keys) + 1:
+                return self.children[index].search_object(key)
+
+        return False
+
+    def search_page(self, key):
+
+        page_obj = self.search_object(key)
+        if page_obj:
+            for pair in page_obj.keys:
+                if pair[0] == key:
+                    return pair[1]
+        return False
+
+    def search_all_after(self, key):
+
+        fst_page = self.search_object(key)
+        el_idx = [pair[0] for pair in fst_page.keys].index(key)
+        lst_of_pages = [pair[1] for pair in fst_page.keys[el_idx:]]
+
+        current_page = fst_page.next_sibling
+        while True:
+            if current_page is None or current_page is False:
+                break
+            lst_of_pages.append([pair[1] for pair in current_page.keys])
+            current_page = current_page.next_sibling
+
+        return lst_of_pages
 
 
 class BPlusTree:
@@ -128,7 +155,10 @@ class BPlusTree:
         self.root = self.root.insert(key)
 
     def search(self, key):
-        return self.root.search(key)
+        return self.root.search_page(key)
+
+    def search_after(self, key):
+        return self.root.search_all_after(key)
 
     def print_tree(self):
         if self.root is None:
@@ -154,46 +184,54 @@ class BPlusTree:
 def main():
     B = BPlusTree()
 
-    names = [
-        "Ковальчук",
-        "Шевченко",
-        "Петренко",
-        "Коваль",
-        "Мельник",
-        "Бондаренко",
-        "Лисенко",
-        "Кравченко",
-        "Сидоренко",
-        "Григоренко",
-        "Іваненко",
-        "Павленко",
-        "Ткаченко",
-        "Іванов",
-        "Поліщук",
-        "Гончаренко",
-        "Романенко",
-        "Дмитренко",
-        "Семененко",
-        "Василенко",
-        "Ярошенко",
-        "Олійник",
-        "Кучеренко",
-        "Черненко",
-        "Гладченко",
-        "Шевельов",
-        "Гаврилюк",
-        "Поляков",
-        "Кондратенко",
-        "Карпенко"
+    names_with_numbers = [
+        ("Соколовський Ігор Петрович", ["+380123456789", "+380987654321"]),
+        ("Козловський Андрій Володимирович", ["+380234567890"]),
+        ("Павлюченко Олександр Олександрович", ["+380345678901", "+380876543210"]),
+        ("Поляковський Михайло Іванович", ["+380456789012"]),
+        ("Коваленко Ольга Сергіївна", ["+380567890123", "+380765432109"]),
+        # ("Міщенко Тетяна Вікторівна", ["+380678901234"]),
+        # ("Івановський Валерій Петрович", ["+380789012345", "+380654321098"]),
+        # ("Сидорович Юрій Віталійович", ["+380890123456"]),
+        # ("Петрович Ігор Васильович", ["+380901234567", "+380543210987"]),
+        # ("Мельничук Ірина Андріївна", ["+380012345678"]),
+        # ("Григорович Наталія Олександрівна", ["+380123456789", "+380432109876"]),
+        # ("Ткаченко Олексій Сергійович", ["+380234567890"]),
+        # ("Шевченко Віктор Миколайович", ["+380345678901", "+380321098765"]),
+        # ("Бондаренко Оксана Петрівна", ["+380456789012"]),
+        # ("Лисенко Василь Ігорович", ["+380567890123"]),
+        # ("Кравченко Олена Володимирівна", ["+380678901234"]),
+        # ("Сидоренко Владислав Олексійович", ["+380789012345", "+380210987654"]),
+        # ("Гриценко Іван Юрійович", ["+380890123456"]),
+        # ("Іванченко Тетяна Михайлівна", ["+380901234567"]),
+        # ("Павленко Дмитро Олегович", ["+380012345678"]),
+        # ("Ткачук Людмила Вікторівна", ["+380123456789", "+380654321098"]),
+        # ("Іваненко Юлія Олегівна", ["+380234567890"]),
+        # ("Петренко Олександр Миколайович", ["+380345678901"]),
+        # ("Михайленко Вікторія Володимирівна", ["+380456789012", "+380987654321"]),
+        # ("Дмитренко Ігор Олександрович", ["+380567890123"]),
+        # ("Василенко Ірина Анатоліївна", ["+380678901234"]),
+        # ("Кондратенко Олександр Вікторович", ["+380789012345"]),
+        # ("Карпенко Юрій Петрович", ["+380890123456"]),
+        # ("Коваленко Олексій Андрійович", ["+380901234567"]),
+        # ("Литвиненко Надія Миколаївна", ["+380012345678"]),
+        # ("Романенко Марина Сергіївна", ["+380123456789"]),
+        # ("Шевельов Сергій Валерійович", ["+380234567890"]),
+        # ("Гончаренко Ірина Петрівна", ["+380345678901"]),
+        # ("Семененко Олег Михайлович", ["+380456789012"]),
+        # ("Черненко Оксана Іванівна", ["+380567890123"]),
+        # ("Гладченко Віталій Володимирович", ["+380678901234"]),
     ]
 
-    for name in names:
+    for name, number in names_with_numbers:
         hashed_name = hash_function(name)
-        print(f"Ім'я: {name}, Хеш-код: {hashed_name}")
+        print(f"Ім'я: {name},\nНомер телефону: {number}.\nХеш-код: {hashed_name}\n")
 
-        B.insert((hashed_name, name))
+        B.insert((hashed_name, (name, number)))
 
     B.print_tree()
+
+    print(B.search_after(hash_function("Соколовський")))
 
 
 if __name__ == '__main__':
